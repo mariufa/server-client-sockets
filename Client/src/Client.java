56f42878faa676bee480eb29e2863f03d55ca15c
mariufa <mariusfagerland@hotmail.com>
2015-12-23 15:22:48 +0100

    Client implemented. Not tested

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,8 +1,41 @@
+import java.awt.image.ImagingOpException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Client {
 	
+	private Socket socket;
+	private BufferedReader input;
+	private PrintWriter output;
+	
 	public void listenSocket() {
-		
+		try {
+			socket = new Socket("Marius", 4321);
+			output = new PrintWriter(socket.getOutputStream());
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: Marius");
+		    System.exit(1);
+		} catch (IOException e) {
+			System.out.println("No I/O");
+		    System.exit(1);
+		}
 	}
-
+	
+	public void sendAndReceive() {
+		output.println("Dette er marius");
+		
+		try {
+			String line = input.readLine();
+			System.out.println("Text received: " + line);
+		} catch (IOException e) {
+			System.out.println("Read failed");
+		    System.exit(1);
+		}
+	}
 }
